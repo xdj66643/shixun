d4169fc9408280009a1a979c76ed638dcd083c7b
xdj66643 <142580490+xdj66643@users.noreply.github.com>
2025-07-08 11:29:58 +0800

    第二个模块：路面病害智能检测后端
    后端完成，只需要具体功能实现来修改

--- backend/src/main/java/com/example/backend/repository/RoadDefectRepository.java
+++ backend/src/main/java/com/example/backend/repository/RoadDefectRepository.java
@@ -3,5 +3,8 @@ package com.example.backend.repository;
 import com.example.backend.entity.RoadDefect;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoadDefectRepository extends JpaRepository<RoadDefect, Long> {
+    List<RoadDefect> findByVideo_Id(Long videoId);
 }
